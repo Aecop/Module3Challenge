4fd8bb7df2b903375645871d4ffe7dee6174ae6f
Joon Ruiz <jruiz@Joons-MacBook-Pro.local>
2022-11-07 18:43:19 -0800

    Last update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Module3Challenge
-Module 3 Challenge
+
+## Description
+
+This activity was work to complete Module 3 Challenge for the UW boot camp. 
+
+Biggest help that I got to complete this Challenge was history of python learning before the UW Boot camp. With the basic 
+understanding of conditional statment and the loop has helped me to troubleshoot and code to get result of the module.
+
+This project is to ask the user who is need of password for what do they want in the password, and length of the password. 
+It will display when all of the instruction has been cleared by the user. 
+
+## Installation
+
+No Installation is required. This is be published on my github. To see the project please copy and paste the website below. 
+
+## Usage
+
+When using the website, please click on generate password to get instruction to create an password. When done correctly, Randomly generated password 
+will be generated inside the box above the "generate password button".
+
+
+```md
+![alt screenshot](assets/Screen%20Shot%202022-11-07%20at%206.11.04%20PM.png)
+```
+
+Thank you!
--- js/script.js
+++ js/script.js
@@ -5,27 +5,27 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
-
+// This function is called when GenerateBtn is called by user pressing the generate button on the website.
 function generatePassword () {
   var activate = document.getElementById("generate");
   var storePW = ""
   
-
+  // Asking user for the length of the password.
   alert("Password must me no less than 8 characters and no more than 128 characters\nPlease be sure to answer all of the question from the prompt to get your new password");
 
+  // This conditional statment will excute when user requested password length is lower than 8 and higher than 128.
   var length = prompt("Please enter the length of passsword you would like to have./n(min 8 character. max 128 character.\n If you type less than 8 character it will automatically adjust to 8 character. \n If you type more than 128 character, it will adjust to 128 character. ");
   if (+length < 8) {
     length = 8;
   }else if (+length > 128) {
     length = 128;
   }
-
+  // This conditional statments is excuted by the answer that user have written on the prompt of the website. 
   var upperCase = prompt("Include upper case? 'Y or N' ");
   if (upperCase === 'Y') {
     storePW += lettersU[Math.floor(Math.random() * lettersU.length)];
   } 
   
-
 
   var numeric = prompt("Include number? 'Y or N' ");
   if (numeric === 'Y') {
@@ -37,10 +37,12 @@ function generatePassword () {
      storePW += symbols[Math.floor(Math.random() * symbols.length)];
   } 
 
+  //This forloop is to excuted and add lowercase letter to the remaining spots available to the length of the password. 
   for (x = storePW.length; x !== +length; x++) {
     storePW += lettersL[Math.floor(Math.random() * lettersL.length)];
   }
 
+  //This is to shuffle the password to different places so that password of special character, numbers and uppercase is not ordered.
   var shuffled = storePW.split('').sort(function(){return 0.5-Math.random()}).join('');
 
  return shuffled;
